040dfdc57439a158a34afeaacc0713bf111cde53
vanitrainingds <vani.training.ds@gmail.com>
2022-08-23 18:48:07 +0530

    added getters and setters

--- src/com/sd/beans/Role.java
+++ src/com/sd/beans/Role.java
@@ -4,4 +4,24 @@ public class Role {
 	private int roleId;
 	private String name;
 	private String desc;
+	public int getRoleId() {
+		return roleId;
+	}
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
 }
+
